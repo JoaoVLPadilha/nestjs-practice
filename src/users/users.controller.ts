0298ca1b0a327f0fbd04ff18f22335592e3927a9
Joao Padilha <jvlpadilha@outlook.com>
2025-01-09 23:03:25 -0300

    feat: post and patch

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,15 +1,41 @@
-import { Controller, Get, Post, Put, Delete, Patch } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Patch,
+  Param,
+  Query,
+  Body,
+  Headers,
+  ParseIntPipe,
+  DefaultValuePipe,
+  ValidationPipe,
+} from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { GetUserParamDto } from './dtos/get-users-param.dto';
+import { PatchUserDto } from './dtos/patch-user.dto';
 
 @Controller('users')
 export class UsersController {
   @Get('/:id?')
-  public getUsers() {
-    return 'You send a request to users endpoint';
+  public getUsers(
+    @Param() getUserParamDto: GetUserParamDto,
+    @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number,
+    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
+  ) {
+    console.log(getUserParamDto);
+    return `You send a request to users endpoint with id: ${JSON.stringify(getUserParamDto)} and query: ${`${JSON.stringify(limit + ' ' + page)}`}`;
   }
 
   @Post()
-  public createUser() {
-    return 'You send a request to create a user';
+  public createUser(
+    @Body() createUserDto: CreateUserDto,
+    @Headers() headers: any,
+  ) {
+    console.log(createUserDto instanceof CreateUserDto);
+    return `You send a request to create a user with body: ${JSON.stringify(createUserDto)}, and headers: ${JSON.stringify(headers)}`;
   }
 
   @Put()
@@ -23,7 +49,7 @@ export class UsersController {
   }
 
   @Patch()
-  public patchUser() {
-    return 'You send a request to patch a user';
+  public patchUser(@Body() patchUserDto: PatchUserDto) {
+    return patchUserDto;
   }
 }
