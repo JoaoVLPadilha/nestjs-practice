ca00f981726b522664043d7fc4515825ac4eb778
Joao Padilha <jvlpadilha@outlook.com>
2025-03-01 18:10:37 -0300

    feat: added advanced pagination to post find all

--- src/common/pagination/providers/pagination.provider.ts
+++ src/common/pagination/providers/pagination.provider.ts
@@ -3,6 +3,7 @@ import { ObjectLiteral, Repository } from 'typeorm';
 import { PaginationQueryDto } from '../dtos/pagination-query.dto';
 import { Request } from 'express';
 import { REQUEST } from '@nestjs/core';
+import { Paginated } from '../interfaces/pagination.interface';
 
 @Injectable()
 export class PaginationProvider {
@@ -13,13 +14,12 @@ export class PaginationProvider {
   public async paginationQuery<T extends ObjectLiteral>(
     paginationQuery: PaginationQueryDto,
     repository: Repository<T>,
-  ): Promise<T[]> {
+  ): Promise<Paginated<T>> {
     const results = await repository.find({
       take: paginationQuery.limit,
       skip: (paginationQuery.page - 1) * paginationQuery.limit,
     });
-    const baseURL =
-      this.request.protocol + '://' + this.request.headers.host + '/';
+    const baseURL = this.request.protocol + '://' + this.request.headers.host;
     const newUrl = new URL(this.request.url, baseURL);
     console.log(newUrl);
 
@@ -35,6 +35,24 @@ export class PaginationProvider {
       paginationQuery.page === 1
         ? paginationQuery.page
         : paginationQuery.page - 1;
-    return results;
+
+    const finalResponse: Paginated<T> = {
+      data: results,
+      meta: {
+        currentPages: paginationQuery.page,
+        totalItems: totalItems,
+        items: results.length,
+        totalPages: totalPages,
+      },
+      links: {
+        current: `${baseURL + newUrl.pathname + newUrl.search}`,
+        first: `${baseURL + newUrl.pathname}${'?limit=' + paginationQuery.limit + '&page=1'}`,
+        last: `${baseURL + newUrl.pathname}${'?limit=' + paginationQuery.limit + '&page=' + totalPages}`,
+        next: `${baseURL + newUrl.pathname}${'?limit=' + paginationQuery.limit + '&page=' + nextPage}`,
+        previous: `${baseURL + newUrl.pathname}${'?limit=' + paginationQuery.limit + '&page=' + previousPage}`,
+      },
+    };
+
+    return finalResponse;
   }
 }
