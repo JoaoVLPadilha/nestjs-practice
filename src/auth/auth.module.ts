43d9a24d07924fe5fe5da5033fc6b4f1c50a6742
Joao Padilha <jvlpadilha@outlook.com>
2025-05-14 23:37:51 -0300

    feat: generate token

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,14 +5,25 @@ import { UsersModule } from 'src/users/users.module';
 import { HashingProvider } from './providers/hashing.provider';
 import { BcryptProvider } from './providers/bcrypt.provider';
 import { SignInProvider } from './providers/sign-in.provider';
+import { ConfigModule } from '@nestjs/config';
+import jwtConfig from './config/jwt.config';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService, {
-    provide: HashingProvider,
-    useClass: BcryptProvider
-  }, SignInProvider],
+  providers: [
+    AuthService,
+    {
+      provide: HashingProvider,
+      useClass: BcryptProvider,
+    },
+    SignInProvider,
+  ],
+  imports: [
+    forwardRef(() => UsersModule),
+    ConfigModule.forFeature(jwtConfig),
+    JwtModule.registerAsync(jwtConfig.asProvider()),
+  ],
   exports: [AuthService, HashingProvider],
-  imports: [forwardRef(() => UsersModule)],
 })
 export class AuthModule {}
